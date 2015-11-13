bb12bfbac25c817e8217fad18ea256f7e1af2c21
Leng Lee <lenglee1@gmail.com>
2015-11-13 18:00:01 +0000

    tables w rships working

--- views.py
+++ views.py
@@ -1,20 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask.ext.bootstrap import Bootstrap
+from flask_wtf.csrf import CsrfProtect
 from forms import Entry
 from models import db, User, Answer
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 db.init_app(app)
+csrf = CsrfProtect(app)
+
 
 app.config['SECRET_KEY'] = "string"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:dog@localhost/blog'
 
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
 	entry = Entry()
-	return render_template('index.html', form = entry)
+
+	if request.method == 'POST':
+		newUser = User(name = entry.name.data, education = entry.education.data)
+		db.session.add(newUser)	
+		db.session.commit()
+		newAnswer = Answer(blog_post = entry.blog_post.data, temperature = entry.temperature.data, user_id = newUser.user_id)
+		db.session.add(newAnswer)
+		db.session.commit()
+
+		return render_template('blog.html')
+
+	elif request.method == 'GET':
+		return render_template('index.html', form = entry)
 
 @app.route('/testdb')
 def testdb():
